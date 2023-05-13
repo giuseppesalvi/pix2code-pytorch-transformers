feat: correct validation and bleau score calculation

models.py
@@ -3,30 +3,6 @@ import torch.nn as nn
 import torchvision.models as models
 from torch.nn import TransformerDecoder, TransformerDecoderLayer
 
-class Encoder(nn.Module):
-
-    def __init__(self, embedding_size):
-
-        super(Encoder, self).__init__()
-
-        resnet = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
-
-        # Remove the fully connected layers, since we don't need the original resnet classes anymore
-        modules = list(resnet.children())[:-1]
-        self.resnet = nn.Sequential(*modules)
-
-        # Create a new fc layer based on the embedding size
-        self.linear = nn.Linear(
-            in_features=resnet.fc.in_features, out_features=embedding_size)
-        self.BatchNorm = nn.BatchNorm1d(
-            num_features=embedding_size, momentum=0.01)
-
-    def forward(self, images):
-        features = self.resnet(images)
-        features = features.view(features.size(0), -1)
-        features = self.BatchNorm(self.linear(features))
-        return features
-
 
 class Decoder(nn.Module):
 
@@ -74,6 +50,47 @@ class Decoder(nn.Module):
 
         return sampled_ids.squeeze()
 
+class Encoder(nn.Module):
+
+    def __init__(self, embedding_size):
+
+        super(Encoder, self).__init__()
+
+        resnet = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
+
+        # Remove the fully connected layers, since we don't need the original resnet classes anymore
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+
+        # Create a new fc layer based on the embedding size
+        self.linear = nn.Linear(
+            in_features=resnet.fc.in_features, out_features=embedding_size)
+        self.BatchNorm = nn.BatchNorm1d(
+            num_features=embedding_size, momentum=0.01)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.BatchNorm(self.linear(features))
+        return features
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-torch.log(torch.tensor(10000.0)) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
 # Decoder implementation with Transformer 
 class DecoderTransformer(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers, nhead=8, dropout=0.1):
@@ -103,48 +120,22 @@ class DecoderTransformer(nn.Module):
         features = features.repeat(1, target_seq_len, 1)
         return features
 
-    def sample_greedy(self, features, start_token_id, end_token_id, max_len=100):
-        print("DBG features shape ", features.shape)
-        features = self.preprocess_encoder_features(features, max_len)
-        print("DBG features shape ", features.shape)
+    def greedy_search(self, features, start_token_id, end_token_id, max_len=100):
         batch_size = features.size(0)
-        sampled_ids = [torch.ones(batch_size, 1).fill_(start_token_id).long().to(features.device)]
-        print("DBG sampled_ids ", sampled_ids)
-        inputs = sampled_ids[-1]
-        print("DBG inputs ", inputs)
+        generated_sequences = torch.full((batch_size, 1), start_token_id, dtype=torch.long, device=features.device)
 
         for _ in range(max_len):
-            tgt = self.embed(inputs)
-            tgt = self.pos_enc(tgt)
-            output = self.transformer_decoder(tgt, features)
-            output = self.linear(output)
-            predicted = output.max(dim=2, keepdim=True)[1]
-            sampled_ids.append(predicted)
-            inputs = predicted
+            output = self.forward(features, generated_sequences)
+            _, next_words = torch.max(output[:, -1, :], dim=-1)
+            next_words = next_words.unsqueeze(1)
+            generated_sequences = torch.cat((generated_sequences, next_words), dim=1)
 
-            if (predicted.squeeze() == end_token_id).all():
+            # Check if all sequences have reached the end_token_id
+            if torch.all((next_words == end_token_id).view(-1)):
                 break
 
-        sampled_ids = torch.cat(sampled_ids, dim=1)
-        return sampled_ids.squeeze()
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-torch.log(torch.tensor(10000.0)) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
+        # Remove the initial start_token_id from the generated sequences
+        return generated_sequences[:, 1:]
 
 
 # Original Pix2code models

train.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
     print("Start Training date and time: {}".format(start.strftime("%Y-%m-%d %H:%M:%S")))
 
     # Training the model
-    for epoch in range(1, args.epochs):
+    for epoch in range(1, args.epochs + 1):
         # Training Loop
         if args.model == "lstm" or args.model == "transformer":
             encoder.train()
@@ -156,47 +156,45 @@ if __name__ == "__main__":
             optimizer.step()
 
         # Validation loop
-        #encoder.eval()
-        #decoder.eval()
+        start_val= datetime.datetime.now()
+        print("Start Validation date and time: {}".format(start_val.strftime("%Y-%m-%d %H:%M:%S")))
+        encoder.eval()
+        decoder.eval()
         #val_losses = []
-        #bleu_scores = []
+        bleu_scores = []
 
-        #with torch.no_grad():
-            #for batch_idx, (images, captions, lengths) in enumerate(eval_dataloader):
-                #images = images.to(device)
-                #captions = captions.to(device)
+        with torch.no_grad():
+            for batch_idx, (images, captions, lengths) in enumerate(eval_dataloader):
+                images = images.to(device)
+                captions = captions.to(device)
 #
                 # Generate captions
-                #features = encoder(images)
-                #start_token_id = vocab.get_id_by_token(vocab.get_start_token)
-                #end_token_id = vocab.get_id_by_token(vocab.get_end_token)
-                #padding_token_id = vocab.get_id_by_token(vocab.get_padding_token)
-                #generated_captions = decoder.sample_greedy(features, start_token_id, end_token_id)
-
-                # Calculate validation loss
-                #val_outputs = decoder(features, captions[:, :-1])
-                #val_loss = criterion(val_outputs.view(-1, len(vocab)), captions[:, 1:].flatten())
-                #val_losses.append(val_loss.item())
+                features = encoder(images)
+                start_token_id = vocab.get_id_by_token(vocab.get_start_token())
+                end_token_id = vocab.get_id_by_token(vocab.get_end_token())
+                padding_token_id = vocab.get_id_by_token(vocab.get_padding_token())
+                generated_captions = decoder.greedy_search(features, start_token_id, end_token_id)
 
                 # Calculate BLEU score
-                #smooth_func = SmoothingFunction().method4
-                #for gen_caption, gt_caption in zip(generated_captions, captions):
-                    #gen_caption_text = [vocab.get_token_by_id[word_id] for word_id in gen_caption.cpu().numpy()]
-                    #gt_caption_text = [vocab.get_token_by_id[word_id] for word_id in gt_caption.cpu().numpy()]
-#
-                    ## Remove start, end and padding tokens from the generated and ground truth captions
-                    #list_start_end_padding_tokens = [vocab.get_start_token, vocab.get_end_token, vocab.get_padding_token]
-                    #gen_caption_text = [word for word in gen_caption_text if word not in list_start_end_padding_tokens]
-                    #gt_caption_text = [word for word in gt_caption_text if word not in list_start_end_padding_tokens]
-#
-                    #bleu_score = sentence_bleu([gt_caption_text], gen_caption_text, smoothing_function=smooth_func)
-                    #bleu_scores.append(bleu_score)
-#
-        #avg_val_loss = np.mean(val_losses)
-        #avg_bleu_score = np.mean(bleu_scores)
+                smooth_func = SmoothingFunction().method4
 
-        #print(f"Epoch: {epoch}/{args.epochs}, Loss: {loss.item()}, Val Loss: {avg_val_loss}, Val BLEU Score: {avg_bleu_score}")
-        print(f"Epoch: {epoch}/{args.epochs}, Loss: {loss.item()}")
+                gen_caption_text = [[vocab.get_token_by_id(word_id) for word_id in batch] for batch in generated_captions.tolist()]
+                gt_caption_text = [[vocab.get_token_by_id(word_id) for word_id in batch] for batch in captions.tolist()]
+
+                # Remove start, end and padding tokens from the generated and ground truth captions
+                list_start_end_padding_tokens = [vocab.get_start_token(), vocab.get_end_token(), vocab.get_padding_token()]
+                gen_caption_text= [[word for word in batch if word not in list_start_end_padding_tokens] for batch in gen_caption_text]
+                gt_caption_text= [[word for word in batch if word not in list_start_end_padding_tokens] for batch in gt_caption_text]
+
+
+                for gt_caption, gen_caption in zip(gt_caption_text, gen_caption_text):
+                    bleu_score = sentence_bleu([gt_caption], gen_caption, smoothing_function=smooth_func)
+                    bleu_scores.append(bleu_score)
+
+        #avg_val_loss = np.mean(val_losses)
+        avg_bleu_score = np.mean(bleu_scores)
+
+        print(f"Epoch: {epoch}/{args.epochs}, Loss: {loss.item()}, Val BLEU Score: {avg_bleu_score}")
         if epoch != 0 and epoch % args.save_after_epochs == 0:
             if args.model == "pix2code":
                 save_model(args.models_dir, [vision_model, language_model, decoder], optimizer, epoch, loss, args.batch_size, vocab, args.model)